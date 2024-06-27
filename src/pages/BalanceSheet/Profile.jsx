7522b2bdc397b0f6a6edd675e393fbd0a38d1de3
Brenda <brenda.vmartinez@roatech.com.mx>
2024-06-26 20:14:29 -0600

    profile card

--- src/pages/BalanceSheet/Profile.jsx
+++ src/pages/BalanceSheet/Profile.jsx
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
+import { useAuth } from '../../context/authContext';
 
 import Title from '../../components/Title';
 
 
 
 export default function Profile() {
+  const {user} = useAuth();
+
 
   return (
     <React.Fragment>
-      <Title>Usuario</Title>
+      <Title>Bienvenido</Title>
       <Typography component="p" variant="h4">
-        Nombre
+        {user.nombre}
       </Typography>
       <Typography color="text.secondary" sx={{ flex: 1 }}>
-        Correo
+        {user.email}
       </Typography>
     </React.Fragment>
   );
